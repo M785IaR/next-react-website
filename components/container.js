488a2737a1255cefc813ff901470868dadbda844
M785IaR <131901147+M785IaR@users.noreply.github.com>
2023-11-17 14:42:59 +0900

    mark-up
    直し（components/container.js）

--- components/container.js
+++ components/container.js
@@ -3,7 +3,7 @@ import styles from 'styles/container.module.css'
 export default function Container({ children, large = false }) {
     return (
         <div className={large ? styles.large : styles.default}>
-            {chileren}
+            {children}
         </div>
     )
 }
