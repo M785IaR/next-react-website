0ba62edcb608fb3c3951b872a032bdc399c5bbb4
M785IaR <131901147+M785IaR@users.noreply.github.com>
2023-12-08 13:39:30 +0900

    mark up
    ~p.269

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from "components/hero"
 
-export default function Home() {
+export default function Blog({ posts }) {
     return (
         <Container>
             <Meta />
         <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
     </Container>
     );
+}
+
+export async function getStaticProps() {
+    const posts = await getAllPosts()
+
+    return {
+        props: {
+            posts: posts,
+        },
+    }
 }
