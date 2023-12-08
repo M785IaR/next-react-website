8fee76b5c1fd429734d9d2327cf6461e675dc015
M785IaR <131901147+M785IaR@users.noreply.github.com>
2023-12-08 13:59:47 +0900

    mark up
    ~p.274

---
+++ components/posts.js
@@ -0,0 +1,34 @@
+import styles from 'styles/posts.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import { eyecatch } from 'images/about.jpg';
+
+export default function Posts({ posts }) {
+    return (
+        <div className={styles.gridContainer}>
+            {posts.map(({ title, slug, eyecatch }) => {
+                return (
+                    <article className={styles.post} key={slug}>
+                        <Link href={`/blog/${slug}`}>
+                            <a>
+                                <figure>
+                                    <Image
+                                        src={eyecatch.url}
+                                        alt=""
+                                        layout="responsive"
+                                        width={eyecatch.width}
+                                        height={eyecatch.height}
+                                        sizes="(min-width: 1152px) 576px, 50vw"
+                                        placeholder="blur"
+                                        blurDataURL={eyecatch.blurDataURL}
+                                    />
+                                </figure>
+                                <h2>{title}</h2>
+                            </a>
+                        </Link>
+                    </article>
+                );
+            })}
+        </div>
+    )
+}
--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 export default async function handler(req, res) {
     if (res.query.select !== process.env.SELECT_TOKEN) {
-        return res.status(401).json({ message: 'Invalid token'})
+        return res.status(401).json({ message: 'Invalid token' })
     }
     try {
         await res.unstable_revalidate('/blog/shedule')
         await res.unstable_revalidate('/blog/music')
-        return res.json({ revalidated: true})
+        return res.json({ revalidated: true })
     } catch (err) {
         return res.status(500).send('Error revalidating')
-=======
-export default async function handler(req, res){
-    try{
-        await res.unstable_revalidate('/blog/shedule')
-        await res.unstable_revalidate('/blog/music')
-        return res.json({ revalidated: true })
-    }catch (err){
-    return res.status(500).send('Error revalidating')
->>>>>>> 65c05ea29d5f0ab3b18dfc06f2534291c50ee91c
     }
 }
